9537abda730262761714a4bd7b7f80bfaa5b4d3f
sotongjan <janicelimlq@gmail.com>
2023-03-09 01:50:27 +0800

    update volunteer control and controller class

--- YouthActionDotNet/Controllers/VolunteerController.cs
+++ YouthActionDotNet/Controllers/VolunteerController.cs
@@ -95,6 +95,16 @@ namespace YouthActionDotNet.Controllers
             return await volunteerControl.All();
         }
 
+        [HttpPost("All")]
+        public async Task<ActionResult<string>> All([FromBody] SearchRequest request)
+        {
+            List<Tag> tags = request.data;
+            int page = request.pageData.page;
+            int pageSize = request.pageData.pageSize;
+            
+            return await volunteerControl.AllInPages(tags, null, page, pageSize);
+        }
+
         [HttpGet("Settings")]
         public string Settings()
         {
